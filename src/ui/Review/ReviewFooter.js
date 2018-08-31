eb571aaa3dec16beb5495df321c498f7141ecc9c
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-08-31 15:31:41 +0100

    Give the footer a bit of space at the bottom

--- src/ui/Review/ReviewFooter.js
+++ src/ui/Review/ReviewFooter.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import ReviewMenu from './ReviewMenu';
+import ReviewDate from './ReviewDate';
 import ReviewLikes from './ReviewLikes';
 import ReviewTitle from './ReviewTitle';
 import ReviewRating from './ReviewRating';
 import ReviewSummary from './ReviewSummary';
 
 const ReviewFooter = () => (
-  <View>
+  <View style={styles.container}>
     <View style={styles.mainActionsAndRating}>
       <ReviewMenu />
       <ReviewRating />
@@ -16,10 +17,15 @@ const ReviewFooter = () => (
     <ReviewLikes />
     <ReviewTitle />
     <ReviewSummary />
+    <ReviewDate />
   </View>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingBottom: 10
+  },
   mainActionsAndRating: {
     height: 56,
     alignItems: 'center',
