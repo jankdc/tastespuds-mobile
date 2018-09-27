dbc385f7a76f650ce3452e6d50fb31fad2aa4d8b
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-09-27 14:21:36 +0100

    Adds register actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,6 +6,10 @@ export const LOGIN = 'tastespuds/user/LOGIN'
 export const LOGIN_FAILED = 'tastespuds/user/LOGIN_FAILED'
 export const LOGIN_PASSED = 'tastespuds/user/LOGIN_PASSED'
 
+export const REGISTER = 'tastespuds/user/REGISTER'
+export const REGISTER_FAILED = 'tastespuds/user/REGISTER_FAILED'
+export const REGISTER_PASSED = 'tastespuds/user/REGISTER_PASSED'
+
 export const VERIFY = 'tastespuds/user/VERIFY'
 export const VERIFY_FAILED = 'tastespuds/user/VERIFY_FAILED'
 export const VERIFY_PASSED = 'tastespuds/user/VERIFY_PASSED'
