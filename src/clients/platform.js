8c68dc96d45a45e7f68f322e102d5f4881243f7b
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-09-30 11:14:31 +0100

    Corrects endpoint

--- src/clients/platform.js
+++ src/clients/platform.js
@@ -8,52 +8,12 @@ export async function login (body) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      email: body.email
-    })
-  })
-
-  if (!response.ok) {
-    throw new Error('Failed to login the user')
-  }
-
-  return response.json()
-}
-
-export async function verify (body) {
-  const response = await window.fetch(`${PLATFORM_DOMAIN}/oauth/verify`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email: body.email,
       code: body.code
     })
   })
 
   if (!response.ok) {
-    throw new Error('Failed to verify the user')
-  }
-
-  return response.json()
-}
-
-export async function register (body) {
-  const response = await window.fetch(`${PLATFORM_DOMAIN}/oauth/register`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: body.name,
-      email: body.email
-    })
-  })
-
-  if (!response.ok) {
-    throw new Error('Failed to register the user')
+    throw new Error('Failed to login the user')
   }
 
   return response.json()
