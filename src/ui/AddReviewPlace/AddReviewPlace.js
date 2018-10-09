b9f630be161c1291dd4744f040bc0cb7b10fbb3f
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-09 09:33:04 +0100

    Adds existing add review place interface

--- src/ui/AddReviewPlace/AddReviewPlace.js
+++ src/ui/AddReviewPlace/AddReviewPlace.js
@@ -1,3 +1,49 @@
-import { View } from 'react-native'
+import React from 'react'
+import {
+  StyleSheet,
+  TextInput,
+  Button,
+  View
+} from 'react-native'
 
-export default View
+class AddReviewPlace extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      place: null
+    }
+  }
+
+  componentDidMount () {
+
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={this._onChangeText}
+          disabled
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  input: {
+
+  }
+})
+
+AddReviewPlace.navigationOptions = {
+  headerLeft: (props) => <Button {...props} title='Cancel' />,
+  title: '🏘️ Place'
+}
+
+export default AddReviewPlace
