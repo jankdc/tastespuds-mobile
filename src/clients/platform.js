2e791c3b45468aae825aef1f89c6940e542bd315
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-10 17:25:37 +0100

    Adds searchPlace to platform client

--- src/clients/platform.js
+++ src/clients/platform.js
@@ -1,4 +1,5 @@
 import { PLATFORM_DOMAIN } from 'react-native-dotenv'
+import { toQueryString } from '../utils/query'
 
 export async function login (code) {
   const response = await window.fetch(`${PLATFORM_DOMAIN}/oauth/login`, {
@@ -31,6 +32,27 @@ export async function updateUser (id, body) {
 
   if (!response.ok) {
     throw new Error('Failed to update the user')
+  }
+
+  return response.json()
+}
+
+export async function searchPlace (name, location) {
+  const queryStr = toQueryString({
+    name,
+    location
+  })
+
+  const response = await window.fetch(`${PLATFORM_DOMAIN}/place` + queryStr, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to search any place')
   }
 
   return response.json()
