ca5b8541b6cfc8e4be1c91b21d30599d7768b322
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-10 17:25:48 +0100

    Adds search place redux actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -10,3 +10,8 @@ export const LOGIN_CANCELLED = 'tastespuds/auth/LOGIN_CANCELLED'
 export const CREATE_USERNAME = 'tastespuds/auth/CREATE_USERNAME'
 export const CREATE_USERNAME_PASSED = 'tastespuds/auth/CREATE_USERNAME_PASSED'
 export const CREATE_USERNAME_FAILED = 'tastespuds/auth/CREATE_USERNAME_FAILED'
+
+// add review
+export const SEARCH_PLACE = 'tastespuds/add-review/SEARCH_PLACE'
+export const SEARCH_PLACE_FAILED = 'tastespuds/add-review/SEARCH_PLACE_FAILED'
+export const SEARCH_PLACE_PASSED = 'tastespuds/add-review/SEARCH_PLACE_PASSED'
