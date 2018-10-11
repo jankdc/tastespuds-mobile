b0e60a402fb32dd29d6d8be16533ab33049173ca
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-11 12:23:51 +0100

    Filter out non foodie/drinkie places

--- src/store/saga/places.js
+++ src/store/saga/places.js
@@ -3,22 +3,32 @@ import { put, call, takeLatest } from 'redux-saga/effects'
 import * as platform from '../../clients/platform'
 import * as actions from '../actions'
 
+const foodiePlaces = new Set([
+  'restaurant',
+  'bakery',
+  'food',
+  'cafe',
+  'bar'
+])
+
 export function * searchPlace ({ value }) {
   try {
     const { status } = yield call(Permissions.askAsync, Permissions.LOCATION)
 
     if (status !== 'granted') {
-      return yield put({
-        type: actions.SEARCH_PLACE_FAILED,
-        error: 'Needs permission to get current user location'
-      })
+      throw new Error('Needs permission to get current user location')
     }
 
     const { coords } = yield call(Location.getCurrentPositionAsync)
     const location = `${coords.latitude},${coords.longitude}`
     const places = yield call(platform.searchPlace, value.name, location)
 
-    yield put({ type: actions.SEARCH_PLACE_PASSED, value: places })
+    yield put({
+      type: actions.SEARCH_PLACE_PASSED,
+      value: places.filter(p =>
+        p.types.filter(pt => foodiePlaces.has(pt)).length > 0
+      )
+    })
   } catch (error) {
     yield put({ type: actions.SEARCH_PLACE_FAILED, error })
   }
