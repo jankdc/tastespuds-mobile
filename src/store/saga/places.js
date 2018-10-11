8562fa7bbc1a1403e80ce52b9cfb1a793b5b9c93
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-11 12:09:56 +0100

    Get user location properly

--- src/store/saga/places.js
+++ src/store/saga/places.js
@@ -1,10 +1,23 @@
+import { Location, Permissions } from 'expo'
 import { put, call, takeLatest } from 'redux-saga/effects'
 import * as platform from '../../clients/platform'
 import * as actions from '../actions'
 
-export function * searchPlace ({ name, location }) {
+export function * searchPlace ({ value }) {
   try {
-    const places = yield call(platform.searchPlace, name, location)
+    const { status } = yield call(Permissions.askAsync, Permissions.LOCATION)
+
+    if (status !== 'granted') {
+      return yield put({
+        type: actions.SEARCH_PLACE_FAILED,
+        error: 'Needs permission to get current user location'
+      })
+    }
+
+    const { coords } = yield call(Location.getCurrentPositionAsync)
+    const location = `${coords.latitude},${coords.longitude}`
+    const places = yield call(platform.searchPlace, value.name, location)
+
     yield put({ type: actions.SEARCH_PLACE_PASSED, value: places })
   } catch (error) {
     yield put({ type: actions.SEARCH_PLACE_FAILED, error })
