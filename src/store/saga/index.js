60828e5077aa5c5dabaf95aeaa0d131c72dfe2ef
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-18 17:08:05 +0100

    Adds reviews saga

--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -4,12 +4,14 @@ import bootSaga from './boot'
 import authSaga from './auth'
 import itemsSaga from './items'
 import placesSaga from './places'
+import reviewsSaga from './reviews'
 
 export default function * rootSaga () {
   yield all([
     call(bootSaga),
     call(authSaga),
     call(itemsSaga),
-    call(placesSaga)
+    call(placesSaga),
+    call(reviewsSaga)
   ])
 }
---
+++ src/store/saga/reviews.js
@@ -0,0 +1,43 @@
+import { put, call, takeLatest } from 'redux-saga/effects'
+import { SecureStore } from 'expo'
+import jwtDecode from 'jwt-decode'
+
+import { navigate } from '../../nav/NavigationService'
+import * as platform from '../../clients/platform'
+import * as actions from '../actions'
+
+export function * addReview ({ value }) {
+  try {
+    const idToken = yield call(SecureStore.getItemAsync, 'idToken')
+    const profile = yield call(jwtDecode, idToken)
+
+    const asset = yield call(platform.addAsset, {
+      uri: value.imageData.uri,
+      type: 'image/jpg',
+      options: {
+        width: `${value.imageData.width}`,
+        height: `${value.imageData.height}`
+      }
+    })
+
+    const review = yield call(platform.addReview, {
+      item: value.item.id === undefined
+        ? value.item
+        : value.item.id,
+      rating: value.rating,
+      assets: [asset.id],
+      userId: profile.sub,
+      content: value.content
+    })
+
+    yield put({ type: actions.ADD_REVIEW_PASSED, value: review })
+    yield call(navigate, 'AppTab')
+  } catch (error) {
+    console.log(error)
+    yield put({ type: actions.ADD_REVIEW_FAILED, error })
+  }
+}
+
+export default function * reviewsSaga () {
+  yield takeLatest(actions.ADD_REVIEW, addReview)
+}
