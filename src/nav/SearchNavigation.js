e3269273c969e86e38eaadf21a72b3255d4dfb68
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-30 10:59:23 +0000

    Integrate search bar

--- src/nav/SearchNavigation.js
+++ src/nav/SearchNavigation.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
-import { createStackNavigator } from 'react-navigation'
+import { SearchBar } from 'react-native-elements'
+import { StyleSheet, Platform } from 'react-native'
+import { SafeAreaView, createStackNavigator } from 'react-navigation'
 
 import Search from '../ui/Search'
 
 const SearchNavigation = createStackNavigator({
   Search: {
-    screen: Search
+    screen: Search,
+    navigationOptions: {
+      header: () => (
+        <SafeAreaView style={styles.headerContainer}>
+          <SearchBar
+            platform={Platform.OS}
+            placeholder='Search'
+            containerStyle={styles.searchBar}
+            cancelButtonTitle='Cancel'
+          />
+        </SafeAreaView>
+      )
+    }
   }
 })
 
 SearchNavigation.navigationOptions = {
   tabBarIcon: ({ tintColor }) => {
     return <Feather name='search' size={26} color={tintColor} />
-  }
+  },
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: 'white'
+  },
+  searchBar: {
+    backgroundColor: 'white'
+  }
+})
 
 export default SearchNavigation
