07c1ce7b5687e53e0e38f065dcbf5bc677574554
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-12-03 13:39:22 +0000

    Fixes review header spacing

--- src/ui/Review/ReviewHeader.js
+++ src/ui/Review/ReviewHeader.js
@@ -5,31 +5,28 @@ const ReviewHeader = ({ review }) => (
   <View style={styles.container}>
     <View style={{ flexDirection: 'column' }}>
       <Text style={styles.name}>{review.item.name}</Text>
-      <Text style={styles.place}>{review.place.name}</Text>
+      <Text style={styles.place} numberOfLines={1} ellipsizeMode='tail'>
+        {review.place.name} - <Text style={styles.city}>{review.place.city}</Text>
+      </Text>
     </View>
   </View>
 )
 
 const styles = StyleSheet.create({
   container: {
-    height: 60,
     alignItems: 'center',
     flexDirection: 'row',
+    paddingVertical: 10,
+    paddingHorizontal: 12,
     backgroundColor: 'white'
   },
-  image: {
-    marginLeft: 10,
-    height: 30,
-    width: 30
-  },
   name: {
-    marginLeft: 10,
     fontSize: 16
   },
   place: {
-    marginLeft: 10,
     fontWeight: 'bold',
-    fontSize: 12
+    fontSize: 12,
+    marginTop: 5
   }
 })
 
