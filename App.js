0b86d196d18f2f484c5adf5ea658ba08c06f6716
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-12-04 13:23:11 +0000

    Set the status bar so that we can see it

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { StatusBar } from 'react-native'
 
 import store from './src/store'
 import BootNavigation from './src/nav/BootNavigation'
 import { setTopLevelNavigator } from './src/nav/NavigationService'
 
 class App extends React.Component {
+  componentDidMount () {
+    StatusBar.setBarStyle('light-content')
+  }
+
   render () {
     return (
       <Provider store={store}>
