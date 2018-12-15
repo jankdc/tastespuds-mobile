26b3acc8eb8aae905b8e83062ce09bd01cf15699
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-12-15 20:35:51 +0000

    Remove undefined keys

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,5 +1,6 @@
 export function toQueryString (params) {
   return '?' + Object.entries(params)
+    .filter(([key, value]) => value !== undefined)
     .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
     .join('&')
 }
